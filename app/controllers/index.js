56ff495e57f8befc3db10f3c8d066dee45e16a5b
Erica Peharda <ericapeharda@gmail.com>
2015-04-29 15:44:29 -0600

    added the certificate file

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,20 +1,19 @@
 function loginUser(){
-	//variable Cloud gets the module ti.Cloud
-	var Cloud = require('ti.Cloud');
+	//variable https gets the module appcelerator.https
+	var https = require("appcelerator.https");
 	
 	//function in the module called User.login
 	//checks if it is a valid user with the cloud service
-	Cloud.Users.login({
-		login: $.txtUserName.value,
-		password: $.txtPwd.value
-	}, function (result){
-		if(result.success){
-			var user = result.users[0]; 
-			alert('Login worked for ' + user.first_name + ' ' + user.last_name + '!');
-		}else{
-			alert('Login failed');
-		}
-	});
+	
+	
+	var securityManager = https.createX509CertificatePinningSecurityManager([
+    {
+        url: "http://coelm2/TNdevelopment/mobile/EricaTest.nsf/HelloWorld.xsp", //URL of the server to contact
+        serverCertificate: "CGA.der" // X.509 certificate in DER format to verify the server's identity'
+    }
+	]);
+	
+
 }
 
 function openNewUser(){
