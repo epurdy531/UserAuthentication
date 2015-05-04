6391ca6412bb046ff3898f70dbfd287716d178b1
Erica Peharda <ericapeharda@gmail.com>
2015-05-04 15:14:16 -0600

    updated index.js file

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,40 +1,34 @@
 function loginUser(){
+	var username = $.txtUserName.value;
+	var password = $.txtPwd.value;
 	var httpClient = Ti.Network.createHTTPClient({
 	    onload: function(e) {
 	        Ti.API.info("Received text on load: " + this.responseText);
-
+	        if (this.responseText.indexOf("Server Login") > -1){
+	        	$.txtUserName.value = "";
+	        	$.txtPwd.value = "";
+	        	//Ti.Gesture.fireEvent('shake');
+	        	alert("Please enter valid credentials");
+	        }else{
+	        Ti.App.myUser = username;
+	         welcomeUser();
+	        }
 	    },
 	    onerror: function(e) {
 	        Ti.API.error("Error is: " + e.error);
 	    },
 	    timeout : 5000,
-	    // Set this property before calling the `open` method. 
-	    //securityManager: securityManager
 	});
-	var username = $.txtUserName.value;
-	var password = $.txtPwd.value;
+
 	//var url = "https://coelm2/TNdevelopment/mobile/EricaTest.nsf/HelloWorld.xsp";
 	authstr = 'Basic ' + Titanium.Utils.base64encode(username + ':' + password);
 	Ti.API.info(authstr);
 
-	var result = httpClient.setRequestHeader("Authorization", authstr);
-	if(result.Status != 0){
-		//good response
-		Ti.API.info("Good response from login credentials.");	        
-		welcomeUser();
-	}
-	else
-	{
-		//bad response
-		alert("Incorrect credentials, please try again.");
-	}
-	
-	
-	httpClient.setRequestHeader("Content-Type", "text/html; charset=utf-8");
-	
 	httpClient.open("GET","http://www.leg.state.co.us/mobile/EricaTestDB.nsf/Test.xsp");
+	httpClient.setRequestHeader("Authorization", authstr);
+	httpClient.setRequestHeader("Content-Type", "text/html; charset=utf-8");
 	httpClient.send();
-	Ti.App.myUser = username;
+
 
 }
 
